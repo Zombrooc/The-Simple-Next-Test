4e5fc85dee798ffd7f21dde905e6cd783e8bf8e7
Elian Valdez <elian.valdez09@gmail.com>
2021-04-09 13:02:19 -0300

    SVG test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
+
+import particlesConfig from "../config/particles.config.json";
+
+// import Navbar from "../components/Navbar";
+
+import GraficDesign from "../assets/images/grafic-design.svg";
+// import ComputerRepair from "../assets/images/computerRepair.svg";
+
+import {
+  Container,
+  Landing,
+  Content,
+  Title,
+  CardContainer,
+} from "../styles/pages/home.styles";
 
 export default function Home() {
   return (
@@ -8,6 +23,7 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
 
+      <GraficDesign/>
       <main>
         <h1>Hello world!</h1>
       </main>
